e06e2ee55caafa9aaddc09f76ac60c475015db37
Ravikumar M <rmittapa@uncc.edu>
2019-04-08 22:05:02 -0400

    Modified code

--- 7.assignment-mpi-basic/7.assignment-mpi-basic/matvec/mpi_matmul.cpp
+++ 7.assignment-mpi-basic/7.assignment-mpi-basic/matvec/mpi_matmul.cpp
@@ -2,16 +2,20 @@
 #include <cmath>
 #include <cstdlib>
 #include <chrono>
+#include <mpi.h>
+#include <math.h>
+
+using namespace std;
 
 float genA (int row, int col) {
   if (row > col)
-    return 1.;
+    return 1.0;
   else
-    return 0.;
+    return 0.0;
 }
 
 float genx0 (int i) {
-  return 1.;
+  return 1.0;
 }
 
 
@@ -19,103 +23,123 @@ void checkx (int iter, long i, float xval) {
   if (iter == 1) {
     float shouldbe = i;
     if (fabs(xval/shouldbe) > 1.01 || fabs(xval/shouldbe) < .99 )
-      std::cout<<"incorrect : x["<<i<<"] at iteration "<<iter<<" should be "<<shouldbe<<" not "<<xval<<std::endl;
+      cout<<"incorrect : x["<<i<<"] at iteration "<<iter<<" should be "<<shouldbe<<" not "<<xval<<endl;
   }
 
   if (iter == 2) {
     float shouldbe =(i-1)*i/2;
     if (fabs(xval/shouldbe) > 1.01 || fabs(xval/shouldbe) < .99)
-      std::cout<<"incorrect : x["<<i<<"] at iteration "<<iter<<" should be "<<shouldbe<<" not "<<xval<<std::endl;
+      cout<<"incorrect : x["<<i<<"] at iteration "<<iter<<" should be "<<shouldbe<<" not "<<xval<<endl;
   }
 }
 
 //perform dense y=Ax on an n \times n matrix
 void matmul(float*A, float*x, float*y, long n) {
+  
   for (long row = 0; row<n; ++row) {
     float sum = 0;
     
-    for (long col = 0; col<n; ++col) {
+    for (long col = 0; col<n ; ++col) {
       //sum += x[col] *A[row][col]
-      sum += x[col] * A[row*n+col];
+       sum += x[col] * A[row*n+col];
     }
 
-    y[row] = sum;
+       y[row] = sum;
   }
 }
 
-int main (int argc, char*argv[]) {
+int main (int argc, char* argv[]) {
 
   if (argc < 3) {
     std::cout<<"usage: "<<argv[0]<<" <n> <iteration>"<<std::endl;
   }
 
-  bool check = true;
-  
-  long n = atol(argv[1]);
-
-  long iter = atol(argv[2]);
-  
 
   //initialize data
-  float* A = new float[n*n];
+  
+   MPI_Init(&argc,&argv);
 
-  for (long row = 0; row<n; row++) {
-    for (long col=0; col<n; col++) {
-      A[row*n+col] = genA(row, col);
+   long n = atol(argv[1]);
+
+   int iter = atoi(argv[2]);
+   bool check = true;
+
+   int worldrank,np;
+   
+   MPI_Comm_rank(MPI_COMM_WORLD,&worldrank);
+   MPI_Comm_size(MPI_COMM_WORLD,&np);
+
+   int p = sqrt(np);
+   long each_div = n/p; 
+   
+   int row_div = worldrank/p,col_div = worldrank%p;
+   
+   MPI_Comm row_comm;
+   int rowrank;
+   MPI_Comm_split(MPI_COMM_WORLD, row_div, worldrank, &row_comm);
+   MPI_Comm_rank(row_comm,&rowrank);
+
+   MPI_Comm col_comm;
+   int colrank;
+   MPI_Comm_split(MPI_COMM_WORLD, col_div, worldrank, &col_comm);
+   MPI_Comm_rank(col_comm,&colrank);
+
+   float* A = new float[each_div*each_div];
+   long rowstart = (row_div*each_div),colstart = (col_div*each_div);
+   long rowend = rowstart+each_div,colend = colstart+each_div;
+
+  for (long row = rowstart,rset=0; row<rowend; row++,rset++) {
+    for (long col= colstart,cset=0; col<colend; col++,cset++) {
+      A[(rset*each_div)+cset] = genA(row, col);
     }
   }
 
-  // for (long row = 0; row<n; row++) {
-  //   for (long col=0; col<n; col++) {
-  //     std::cout<<A[row*n+col]<<" ";
-  //   }
-  //   std::cout<<std::endl;
-  // }
+  float* x = new float[each_div];
 
-  float* x = new float[n];
-
-  for (long i=0; i<n; ++i)
-    x[i] = genx0(i);
-
-  // for (long i=0; i<n; ++i)
-  //   std::cout<<x[i]<<" ";
-  // std::cout<<std::endl;
-  
-  float* y = new float[n];
-
-  std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
-  
-  for (int it = 0; it<iter; ++it) {
-  
-    matmul(A, x, y, n);
-
+  for (long i=0; i<each_div; i++)
     {
-      float*t = x;
-      x=y;
-      y=t;
+     x[i] = genx0(i);
     }
-
-    // std::cout<<"x["<<it+1<<"]: ";
-    // for (long i=0; i<n; ++i)
-    //   std::cout<<x[i]<<" ";
-    // std::cout<<std::endl;
-
-    if (check)
-      for (long i = 0; i<n; ++i)
-	checkx (it+1, i, x[i]);
+  float* y = new float[each_div];
+  for (long i=0; i<each_div; i++)
+    y[i] = 0.0;
+ 
+   double start = MPI_Wtime(); 
+   // cout<<"y---------------"<<worldrank<<endl;
+   for (int it = 0; it<iter; it++) 
+   {
+      matmul(A,x,y,each_div);
+      /* cout<<"iteration"<<it<<endl;
+       for (long row = 0; row<each_div; row=row+1) {
+         float sum = 0.0;
+    
+          for (long col = 0; col<each_div ; col=col+1) {
+          sum = sum + (x[col] * A[(row*each_div)+col]);
+          }
+             y[row] = sum;
+       }*/
+      MPI_Reduce(y,x,each_div,MPI_FLOAT,MPI_SUM,row_div,row_comm);
+      MPI_Bcast(x,each_div,MPI_FLOAT,col_div, col_comm);
+  if (check){
+    for (long i = colstart,p=0; i<colend; ++i,++p){
+        checkx (it+1, i, x[p]);
+             }
+          }
   }
   
-  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  if(worldrank == 0){
+        double end = MPI_Wtime(); 
+        cerr<<end-start<<endl;
+   }
+ 
+  MPI_Comm_free(&row_comm);
+  MPI_Comm_free(&col_comm);
 
-  std::chrono::duration<double> elapsed_seconds = end-start;
-
-  std::cerr<<elapsed_seconds.count()<<std::endl;
-
-  
-  
   delete[] A;
   delete[] x;
   delete[] y;
-  
+
+  MPI_Finalize();
+ 
   return 0;
 }
--- 7.assignment-mpi-basic/7.assignment-mpi-basic/num_int/mpi_num_int.cpp
+++ 7.assignment-mpi-basic/7.assignment-mpi-basic/num_int/mpi_num_int.cpp
@@ -4,6 +4,8 @@
 #include <chrono>
 #include <mpi.h>
 
+using namespace std;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -25,7 +27,60 @@ int main (int argc, char* argv[]) {
     return -1;
   }
   
+  MPI_Init(&argc,&argv);
+  int fid = atoi(argv[1]);
+  float a = atof(argv[2]);
+  float b = atof(argv[3]);
+  int n = atoi(argv[4]);
+  int intensity = atoi(argv[5]);
+  int rank,p;
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  MPI_Comm_size(MPI_COMM_WORLD,&p);
+  float (*func)(float,int);
+  float limit = (b-a)/n;
+  int itstart,itend,partition;
+  double start = MPI_Wtime(); 
+  partition = n/p;
+  itstart = rank*partition; itend = (rank+1)*partition;
+  if(rank == p-1)
+	  itend = n;
+  switch(fid)
+  {
+     case 1 : func = &f1;
+                
+                break;
 
+     case 2 : func = &f2;
+                break;
 
+     case 3 : func = &f3;
+                break;
+
+     case 4 : func = &f4;
+                break;
+
+     default :  cerr<<"Functionid does not exists, Enter 1, 2, 3 or 4" <<endl; 
+                return -1;                
+
+   };
+   float par_integralval= 0.0,integralval = 0.0;
+   for(int i = itstart ; i<itend ;i++)
+        {
+           float x = a + ( (i + 0.5) *limit );
+	       par_integralval += (*func)(x,intensity); 
+        }
+   MPI_Reduce(&par_integralval,&integralval,1,MPI_FLOAT,MPI_SUM,0,MPI_COMM_WORLD);
+   if(rank == 0)
+   {
+	   integralval = integralval * limit;
+	   cout<<integralval<<endl;
+           double end = MPI_Wtime(); 
+           cerr<<end-start<<endl;
+   }
+   
+  
+   
+   MPI_Finalize();
+   
   return 0;
 }
